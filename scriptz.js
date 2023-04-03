63177d82c0cb00225cfbab17fc7dcc3fbaf176e8
TAGISWILD <chauhan.a.atharvak26@gmail.com>
2023-04-03 11:05:58 +0530

    homescreen

--- scriptz.js
+++ scriptz.js
@@ -1,17 +1,11 @@
-
-
-var splashScreen = document.querySelector('.splash');
-splashScreen.addEventListener('click',()=>{
-  splashScreen.style.opacity = 0;
-  setTimeout(()=>{
-    splashScreen.classList.add('hidden')
-  },610)
-  location.replace('https://4thdimension.live/HP/')
-
-
-})
 window.addEventListener("DOMContentLoaded", event => {
   const audio = document.querySelector("audio");
   audio.volume = 0.8;
   audio.play();
   })  
+
+  var delayInMilliseconds = 6000; //6 second
+
+setTimeout(function() {
+  location.replace('https://4thdimension.live/HP/')
+}, delayInMilliseconds);
